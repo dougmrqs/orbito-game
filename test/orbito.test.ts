f2baab8f6970255c317f7c65ced00bb946be94e1
Douglas Marques <douglas.bmarques@hotmail.com>
2023-10-31 20:10:27 -0300

    add tests to orbito

--- test/orbito.test.ts
+++ test/orbito.test.ts
@@ -25,42 +25,76 @@ describe("Orbito Game", () => {
       });
 
       it("allows player to place a piece after moving other player's piece", () => {
-        // // player 1 places a piece
-        // const orbito = new Orbito();
-        // const space = orbito.game.board.innerOrbit[0];
-        // orbito.play({ toSpace: space });
-        // expect(orbito.game.board.findSpace(space.id).piece).toEqual({
-        //   player: orbito.players[0],
-        // });
-        // // player 2 moves the piece
-        // const space2 = orbito.game.board.innerOrbit[1];
-        // orbito.play({ fromSpace: space, toSpace: space2 });
-        // expect(orbito.game.board.findSpace(space.id).piece).toEqual(null);
-        // expect(orbito.game.board.findSpace(space2.id).piece).toEqual({
-        //   player: orbito.players[0],
-        // });
-        // // player 2 places a piece
-        // const space3 = orbito.game.board.innerOrbit[3];
-        // orbito.play({ toSpace: space3 });
-        // orbito.presentBoard();
-        // expect(orbito.game.board.findSpace(space3.id).piece).toEqual({
-        //   player: orbito.players[1],
-        // });
+        const orbito = new Orbito();
+
+        // player 1 places a piece
+        const firstPlaySpace = orbito.findSpaceById(13);
+        orbito.play({ toSpace: firstPlaySpace });
+
+        // player 2 moves the piece
+        const movePlaySpace = orbito.findSpaceById(14);
+        orbito.play({ fromSpace: firstPlaySpace, toSpace: movePlaySpace });
+
+        // player 2 places a piece
+        const secondPlaySpace = orbito.findSpaceById(13);
+        orbito.play({ toSpace: secondPlaySpace });
+
+        expect(
+          orbito.game.board.findSpaceById(secondPlaySpace.id).piece
+        ).toEqual({
+          player: orbito.players[1],
+        });
       });
 
-      it("does not allow to move other player's piece to a non-adjacent space", () => {});
+      it("does not allow to move other player's piece to a non-adjacent space", () => {
+        const orbito = new Orbito();
+
+        // player 1 places a piece
+        const firstPlaySpace = orbito.findSpaceById(13);
+        orbito.play({ toSpace: firstPlaySpace });
+
+        // player 2 moves the piece
+        // 13 16 -> 16 15
+        // 14 15 -> 13 14
+        // 13 should not be able to move to 15
+        const movePlaySpace = orbito.findSpaceById(15);
+        orbito.play({ fromSpace: firstPlaySpace, toSpace: movePlaySpace });
+
+        expect(orbito.findSpaceById(firstPlaySpace.id).piece).toEqual({
+          player: orbito.players[0],
+        });
+        expect(orbito.findSpaceById(movePlaySpace.id).piece).toEqual(null);
+      });
+
+      it("waits on players turn until it makes a valid move", () => {});
 
       it("finishes players turn and orbits by placing a piece to anywhere disoccupied in the board", () => {
         const orbito = new Orbito();
+
+        expect(orbito.game.board.innerOrbit).toEqual([
+          { id: 13, position: 0, piece: null },
+          { id: 14, position: 1, piece: null },
+          { id: 15, position: 2, piece: null },
+          { id: 16, position: 3, piece: null },
+        ]);
+
+        // player 1 places a piece
+        const firstPlaySpace = orbito.findSpaceById(13);
+        orbito.play({ toSpace: firstPlaySpace });
+
+        expect(orbito.game.board.innerOrbit).toEqual([
+          { id: 13, position: 3, piece: { player: orbito.players[0] } },
+          { id: 14, position: 0, piece: null },
+          { id: 15, position: 1, piece: null },
+          { id: 16, position: 2, piece: null },
+        ]);
       });
     });
 
     describe("phase 2", () => {
       it("allows to place a piece to anywhere disoccupied in the board", () => {});
-    });
-
-    describe("phase 3", () => {
-      it("shifts all the pieces one space forward at the end of the turn", () => {});
+      it("does not allow to place a piece to an occupied space", () => {});
+      it("does not allow a move play", () => {});
     });
   });
 
