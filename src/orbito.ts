ecaa60b8fa7603ab738c909f72cc01458836fda2
Douglas Marques <douglas.bmarques@hotmail.com>
2023-11-03 11:06:41 -0300

    modify board presenter

--- src/orbito.ts
+++ src/orbito.ts
@@ -66,7 +66,34 @@ class Orbito {
   }
 
   presentBoard() {
-    console.log(this.game.board.innerOrbit, this.game.board.outerOrbit);
+    const innerOrbit = this.game.board.innerOrbit;
+    const outerOrbit = this.game.board.outerOrbit;
+    const row1 = [
+      outerOrbit[0].piece?.player.color,
+      outerOrbit[1].piece?.player.color,
+      outerOrbit[2].piece?.player.color,
+      outerOrbit[3].piece?.player.color,
+    ];
+    const row2 = [
+      outerOrbit[11].piece?.player.color,
+      innerOrbit[0].piece?.player.color,
+      innerOrbit[1].piece?.player.color,
+      outerOrbit[4].piece?.player.color,
+    ];
+    const row3 = [
+      outerOrbit[10].piece?.player.color,
+      innerOrbit[3].piece?.player.color,
+      innerOrbit[2].piece?.player.color,
+      outerOrbit[5].piece?.player.color,
+    ];
+    const row4 = [
+      outerOrbit[9].piece?.player.color,
+      outerOrbit[8].piece?.player.color,
+      outerOrbit[7].piece?.player.color,
+      outerOrbit[6].piece?.player.color,
+    ];
+
+    console.log(`${row1}\n${row2}\n${row3}\n${row4}`);
   }
 
   private checkBoardFull() {
